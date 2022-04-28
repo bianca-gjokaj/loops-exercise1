c4f65a665e27b0ed030b0df8f4f8bfab1d995379
bianca <bianca.gjokaj@gmail.com>
2022-04-28 12:31:20 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// for (var i = 1; i <= 10; i++) {
+//         console.log(i)
+// }
+
+// let i = 0;
+// while (i < 10) {
+//     i++
+//     console.log(i)
+// }
+
+
+// let i = 1;
+// const n = 10;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= n);
+
+
+// const array1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+// for (const element of array1) {
+//     console.log(element);
+// }
